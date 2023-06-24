962b2b93969fc555a175ffd33c9c7fd4e9c8d300
Junchi Yu (余俊驰) <42906987+Samyu0304@users.noreply.github.com>
2023-06-24 17:21:16 -0400

    Add files via upload

---
+++ graph-level/run_cfg/train_multigib_joint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+python  ../multigib_main.py --cuda 4 --kld_weight 0.1 --dist_weight 0.1 --lr_a 0.001  --lr 0.001 --dataset SPMotif-0.33 --layers 2 --batch_size 256  --hiddens 64 --K 3 --joint
+python  ../multigib_main.py --cuda 4 --kld_weight 0.1 --dist_weight 0.1 --lr_a 0.001  --lr 0.001 --dataset SPMotif-0.5 --layers 2 --batch_size 256  --hiddens 64 --K 3 --joint
+python  ../multigib_main.py --cuda 4 --kld_weight 0.1 --dist_weight 0.1 --lr_a 0.001  --lr 0.001 --dataset SPMotif-0.7 --layers 2 --batch_size 256  --hiddens 64 --K 3 --joint
+python  ../multigib_main.py --cuda 4 --kld_weight 0.1 --dist_weight 0.1 --lr_a 0.001  --lr 0.001 --dataset SPMotif-0.9 --layers 2 --batch_size 256  --hiddens 64 --K 3 --joint
+python  ../multigib_main.py --cuda 4 --kld_weight 0.1 --dist_weight 0.1 --lr_a 0.001  --lr 0.001 --dataset DD --layers 2 --batch_size 256  --hiddens 64 --K 3 --joint
+python  ../multigib_main.py --cuda 4 --kld_weight 0.1 --dist_weight 0.1 --lr_a 0.001  --lr 0.001 --dataset MUTAG --layers 2 --batch_size 256  --hiddens 64 --K 3 --joint
+python  ../multigib_main.py --cuda 4 --kld_weight 0.1 --dist_weight 0.1 --lr_a 0.001  --lr 0.001 --dataset mnist --layers 2 --batch_size 256  --hiddens 64 --K 3 --joint
